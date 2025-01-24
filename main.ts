9d5371e521a8457d158215928c03059b872c5de9
PoloCotarelo <polo.cotarelo@gmail.com>
2025-01-24 12:28:27 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,32 @@
-basic.forever(function () {
-    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 150)
-    motor.MotorRun(motor.Motors.M2, motor.Dir.CW, 150)
-    basic.pause(2000)
-    motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, 150)
-    motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, 150)
-    basic.pause(2000)
+let espera = 0
+let fila = 0
+let columna = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(randint(11, 99))
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    espera = 50
+    fila = 0
+    columna = 0
+    for (let index = 0; index < 25; index++) {
+        led.toggle(fila, columna)
+        for (let index = 0; index < 10; index++) {
+            motor.stepperDegree_42(motor.Steppers.M1_M2, motor.Dir.CW, 180)
+            basic.pause(espera)
+            motor.stepperDegree_42(motor.Steppers.M1_M2, motor.Dir.CCW, 180)
+            basic.pause(espera)
+        }
+        fila += 1
+        if (fila == 5) {
+            columna += 1
+            fila = 0
+        }
+    }
 })
