6423448d0df99ccf4a13089c5df428ec40f50597
PoloCotarelo <polo.cotarelo@gmail.com>
2025-03-22 09:11:49 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+function Muestra () {
+    while (!(input.logoIsPressed())) {
+        basic.showNumber(objetivo)
+    }
+}
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     for (let index = 0; index < 2; index++) {
         motor.stepperDegree_42(motor.Steppers.M1_M2, motor.Dir.CW, 90)
@@ -23,8 +28,8 @@ input.onButtonPressed(Button.B, function () {
             `)
         juego = 1
         espera = 45
-        fila = 0
-        columna = 0
+        col = 0
+        fil = 0
         for (let index = 0; index < 25; index++) {
             for (let index = 0; index < 2; index++) {
                 motor.stepperDegree_42(motor.Steppers.M1_M2, motor.Dir.CW, 180)
@@ -32,25 +37,26 @@ input.onButtonPressed(Button.B, function () {
                 motor.stepperDegree_42(motor.Steppers.M1_M2, motor.Dir.CCW, 180)
                 basic.pause(espera)
             }
-            led.toggle(fila, columna)
-            fila += 1
-            if (fila == 5) {
-                columna += 1
-                fila = 0
+            led.toggle(fil, col)
+            col += 1
+            if (col == 5) {
+                fil += 1
+                col = 0
             }
-            if (fila == 4 && columna > 1) {
-                music.ringTone(262)
+            if (fil == 4 && col > 1) {
+                music.play(music.tonePlayable(262, music.beat(BeatFraction.Eighth)), music.PlaybackMode.InBackground)
             }
         }
         motor.motorStopAll()
         music.play(music.builtinPlayableSoundEffect(soundExpression.slide), music.PlaybackMode.UntilDone)
         juego = 0
+        Muestra()
     }
 })
-let columna = 0
-let fila = 0
-let objetivo = 0
+let fil = 0
+let col = 0
 let espera = 0
+let objetivo = 0
 let juego = 0
 motor.motorStopAll()
 juego = 0
